d3d17317debd0df01bbc0c87313e930ec74b303c
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-06-28 19:32:06 +0900

    ui: add eslint

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,15 +1,15 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
+  entry: './index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
-    })
+    }),
   ],
 };
